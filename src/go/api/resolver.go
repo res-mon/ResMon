94292897b3d07aa3df9719eaa194608c7226765c
Felix Mayer <FelixM@yer.tools>
2024-02-23 22:28:28 +0100

    Handle subscription

--- src/go/api/resolver.go
+++ src/go/api/resolver.go
@@ -4,15 +4,21 @@ import (
 	"context"
 
 	"github.com/yerTools/ResMon/generated/go/graph"
+	"github.com/yerTools/ResMon/src/go/utility"
 )
-
-var globalActive = false
 
 type resolver graph.Resolver
 
 type rootMutationResolver graph.Resolver
 type rootQueryResolver graph.Resolver
+type rootSubscriptionResolver graph.Resolver
 type activityMutationResolver graph.Resolver
+
+var globalActive = utility.NewSubscribable(&graph.WorkClockQuery{
+	Activity: &graph.ActivityQuery{
+		Active: false,
+	},
+}, 16)
 
 func (r resolver) RootMutation() graph.RootMutationResolver {
 	return rootMutationResolver(r)
@@ -22,29 +28,42 @@ func (r resolver) RootQuery() graph.RootQueryResolver {
 	return rootQueryResolver(r)
 }
 
+func (r resolver) RootSubscription() graph.RootSubscriptionResolver {
+	return rootSubscriptionResolver(r)
+}
+
 func (r resolver) ActivityMutation() graph.ActivityMutationResolver {
 	return activityMutationResolver(r)
 }
 
-func (r activityMutationResolver) SetActive(ctx context.Context, obj *graph.ActivityMutation, active bool) (bool, error) {
-	globalActive = active
+func (activityMutationResolver) SetActive(ctx context.Context, obj *graph.ActivityMutation, active bool) (bool, error) {
+	globalActive.Set(&graph.WorkClockQuery{
+		Activity: &graph.ActivityQuery{
+			Active: active,
+		},
+	})
+
 	return active, nil
 }
 
-func (r rootMutationResolver) WorkClock(ctx context.Context) (*graph.WorkClockMutation, error) {
+func (rootMutationResolver) WorkClock(ctx context.Context) (*graph.WorkClockMutation, error) {
 	return &graph.WorkClockMutation{
 		Activity: &graph.ActivityMutation{
-			SetActive: globalActive,
+			SetActive: globalActive.Current().Activity.Active,
 		},
 	}, nil
 }
 
-func (r rootQueryResolver) WorkClock(ctx context.Context) (*graph.WorkClockQuery, error) {
+func (rootQueryResolver) WorkClock(ctx context.Context) (*graph.WorkClockQuery, error) {
 	return &graph.WorkClockQuery{
 		Activity: &graph.ActivityQuery{
-			Active: globalActive,
+			Active: globalActive.Current().Activity.Active,
 		},
 	}, nil
+}
+
+func (rootSubscriptionResolver) WorkClock(ctx context.Context) (<-chan *graph.WorkClockQuery, error) {
+	return globalActive.Subscribe(ctx), nil
 }
 
 func New() graph.Config {
