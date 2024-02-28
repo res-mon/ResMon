2b76c9b066379f7713f4e5df029501272e1af77d
Felix Mayer <FelixM@yer.tools>
2024-02-28 20:19:55 +0100

    Add lazy subscribable

--- src/go/utility/subscribable.go
+++ src/go/utility/subscribable.go
@@ -2,6 +2,8 @@ package utility
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"sync"
 )
 
@@ -12,10 +14,26 @@ type subscribable[T any] struct {
 	mutex       sync.Mutex
 }
 
-func NewSubscribable[T any](value T, bufferSize int) *subscribable[T] {
-	return &subscribable[T]{
+func initSubscribable[T any](value T, bufferSize int) subscribable[T] {
+	return subscribable[T]{
 		value:      value,
 		bufferSize: bufferSize,
+	}
+}
+
+func NewSubscribable[T any](value T, bufferSize int) *subscribable[T] {
+	res := initSubscribable(value, bufferSize)
+	return &res
+}
+
+func (s *subscribable[T]) Current() T {
+	return s.value
+}
+
+func (s *subscribable[T]) setUnlocked(value T) {
+	s.value = value
+	for _, c := range s.subscribers {
+		c <- value
 	}
 }
 
@@ -23,38 +41,120 @@ func (s *subscribable[T]) Set(value T) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	s.value = value
-	for _, c := range s.subscribers {
-		c <- value
-	}
+	s.setUnlocked(value)
 }
 
-func (s *subscribable[T]) Current() T {
-	return s.value
-}
-
-func (s *subscribable[T]) Subscribe(ctx context.Context) <-chan T {
+func (s *subscribable[T]) subscribeUnlocked(ctx context.Context) <-chan T {
 	ch := make(chan T, s.bufferSize)
-
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
 	s.subscribers = append(s.subscribers, ch)
 
 	go func() {
-		ch <- s.value
+		defer close(ch)
 
+		ch <- s.value
 		<-ctx.Done()
+
 		s.mutex.Lock()
 		defer s.mutex.Unlock()
 		for i, c := range s.subscribers {
 			if c == ch {
-				s.subscribers = append(s.subscribers[:i], s.subscribers[i+1:]...)
-				close(ch)
+				s.subscribers = append(
+					s.subscribers[:i],
+					s.subscribers[i+1:]...)
+
 				return
 			}
 		}
 	}()
 
 	return ch
+}
+
+func (s *subscribable[T]) Subscribe(ctx context.Context) <-chan T {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	return s.subscribeUnlocked(ctx)
+}
+
+type lazySubscribable[T any] struct {
+	subscribable[T]
+	loaded       bool
+	loader       func(ctx context.Context) (T, error)
+	beforeUpdate func(ctx context.Context, oldValue T, newValue T) error
+}
+
+func NewLazySubscribable[T any](
+	loader func(ctx context.Context) (T, error),
+	beforeUpdate func(ctx context.Context, oldValue T, newValue T) error,
+	bufferSize int,
+) (*lazySubscribable[T], error) {
+
+	if loader == nil {
+		return nil, errors.New("loader is required for new lazy subscribable")
+	}
+
+	var value T
+
+	return &lazySubscribable[T]{
+		loader:       loader,
+		beforeUpdate: beforeUpdate,
+		subscribable: initSubscribable(value, bufferSize),
+	}, nil
+}
+
+func (s *lazySubscribable[T]) currentUnlocked(ctx context.Context) (T, error) {
+	if s.loaded {
+		return s.value, nil
+	}
+
+	value, err := s.loader(ctx)
+	if err != nil {
+		return value, fmt.Errorf("could not load value: %w", err)
+	}
+
+	s.value = value
+	s.loaded = true
+
+	return value, nil
+}
+
+func (s *lazySubscribable[T]) Current(ctx context.Context) (T, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	return s.currentUnlocked(ctx)
+}
+
+func (s *lazySubscribable[T]) Set(ctx context.Context, value T) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	oldValue, err := s.currentUnlocked(ctx)
+	if err != nil {
+		return fmt.Errorf("could not get current value: %w", err)
+	}
+
+	if s.beforeUpdate != nil {
+		err := s.beforeUpdate(ctx, oldValue, value)
+		if err != nil {
+			return fmt.Errorf("before update failed: %w", err)
+		}
+	}
+
+	s.setUnlocked(value)
+
+	return nil
+}
+
+func (s *lazySubscribable[T]) Subscribe(ctx context.Context) (<-chan T, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	_, err := s.currentUnlocked(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not get current value: %w", err)
+	}
+
+	return s.subscribeUnlocked(ctx), nil
 }
