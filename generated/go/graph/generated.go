e76933e10b4aae8c1289b43581f18845145f5f7b
Felix Mayer <FelixM@yer.tools>
2024-03-15 01:08:50 +0100

    Update generated code

--- generated/go/graph/generated.go
+++ generated/go/graph/generated.go
@@ -41,6 +41,7 @@ type Config struct {
 
 type ResolverRoot interface {
 	ActivityMutation() ActivityMutationResolver
+	HistoryQuery() HistoryQueryResolver
 	RootMutation() RootMutationResolver
 	RootQuery() RootQueryResolver
 	RootSubscription() RootSubscriptionResolver
@@ -106,6 +107,9 @@ type ComplexityRoot struct {
 
 type ActivityMutationResolver interface {
 	SetActive(ctx context.Context, obj *ActivityMutation, active bool) (*ActivityQuery, error)
+}
+type HistoryQueryResolver interface {
+	HistoryItems(ctx context.Context, obj *HistoryQuery, from *scalar.Timestamp, to *scalar.Timestamp, limit *int) ([]*HistoryItem, error)
 }
 type RootMutationResolver interface {
 	WorkClock(ctx context.Context) (*WorkClockMutation, error)
@@ -949,7 +953,7 @@ func (ec *executionContext) _HistoryQuery_historyItems(ctx context.Context, fiel
 	}()
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
-		return obj.HistoryItems, nil
+		return ec.resolvers.HistoryQuery().HistoryItems(rctx, obj, fc.Args["from"].(*scalar.Timestamp), fc.Args["to"].(*scalar.Timestamp), fc.Args["limit"].(*int))
 	})
 	if err != nil {
 		ec.Error(ctx, err)
@@ -970,8 +974,8 @@ func (ec *executionContext) fieldContext_HistoryQuery_historyItems(ctx context.C
 	fc = &graphql.FieldContext{
 		Object:     "HistoryQuery",
 		Field:      field,
-		IsMethod:   false,
-		IsResolver: false,
+		IsMethod:   true,
+		IsResolver: true,
 		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
 			switch field.Name {
 			case "start":
@@ -3610,10 +3614,41 @@ func (ec *executionContext) _HistoryQuery(ctx context.Context, sel ast.Selection
 		case "__typename":
 			out.Values[i] = graphql.MarshalString("HistoryQuery")
 		case "historyItems":
-			out.Values[i] = ec._HistoryQuery_historyItems(ctx, field, obj)
-			if out.Values[i] == graphql.Null {
-				out.Invalids++
+			field := field
+
+			innerFunc := func(ctx context.Context, fs *graphql.FieldSet) (res graphql.Marshaler) {
+				defer func() {
+					if r := recover(); r != nil {
+						ec.Error(ctx, ec.Recover(ctx, r))
+					}
+				}()
+				res = ec._HistoryQuery_historyItems(ctx, field, obj)
+				if res == graphql.Null {
+					atomic.AddUint32(&fs.Invalids, 1)
+				}
+				return res
 			}
+
+			if field.Deferrable != nil {
+				dfs, ok := deferred[field.Deferrable.Label]
+				di := 0
+				if ok {
+					dfs.AddField(field)
+					di = len(dfs.Values) - 1
+				} else {
+					dfs = graphql.NewFieldSet([]graphql.CollectedField{field})
+					deferred[field.Deferrable.Label] = dfs
+				}
+				dfs.Concurrently(di, func(ctx context.Context) graphql.Marshaler {
+					return innerFunc(ctx, dfs)
+				})
+
+				// don't run the out.Concurrently() call below
+				out.Values[i] = graphql.Null
+				continue
+			}
+
+			out.Concurrently(i, func(ctx context.Context) graphql.Marshaler { return innerFunc(ctx, out) })
 		default:
 			panic("unknown field " + strconv.Quote(field.Name))
 		}
